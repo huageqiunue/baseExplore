55d33be636039e18c1df28eba967bc5e1df6a479
chenjunhua <1625788695@qq.com>
2023-09-17 17:22:03 +0800

    [feature]基础架构

--- app/src/main/java/com/junhua/myapplication/activity/CustomViewActivity.kt
+++ app/src/main/java/com/junhua/myapplication/activity/CustomViewActivity.kt
@@ -3,7 +3,7 @@ package com.junhua.myapplication.activity
 import com.junhua.myapplication.databinding.ActivityCustomViewBinding
 import com.junhua.myapplication.frame.BaseActivity
 
-class CustomVIewActivity : BaseActivity<ActivityCustomViewBinding>(ActivityCustomViewBinding::inflate) {
+class CustomViewActivity : BaseActivity<ActivityCustomViewBinding>(ActivityCustomViewBinding::inflate) {
     override fun ActivityCustomViewBinding.initViews() {
     }
 
