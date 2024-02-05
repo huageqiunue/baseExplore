32675d565ad21a9fc0f37ac3cfada982e9fafef5
junhuachen <1625788695@qq.com>
2024-02-05 16:34:06 +0800

    [feature]Stat

---
+++ app/src/main/java/com/junhua/myapplication/activity/OpencvTestActivity.kt
@@ -0,0 +1,22 @@
+package com.junhua.myapplication.activity
+
+import com.junhua.myapplication.databinding.ActivityOpencvTestBinding
+import com.junhua.myapplication.frame.BaseActivity
+
+/**
+ * 测试openCv的Activity
+ */
+class OpencvTestActivity : BaseActivity<ActivityOpencvTestBinding>(ActivityOpencvTestBinding::inflate) {
+
+    override fun ActivityOpencvTestBinding.initViews() {
+        TODO("Not yet implemented")
+    }
+
+    override fun ActivityOpencvTestBinding.initListener() {
+        TODO("Not yet implemented")
+    }
+
+    override fun ActivityOpencvTestBinding.initConfig() {
+        TODO("Not yet implemented")
+    }
+}
